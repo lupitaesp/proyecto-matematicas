5ae8d76e27c911b7864d1d7622f3010610a89c0d
Lupita Espinoza <1718110388@utectulancingo.gob.mx>
2021-09-23 23:49:42 -0500

    PROGRAMAS DIVISION, MULTIPLICACION, RESTA

---
+++ division.py
@@ -0,0 +1,8 @@
+print("Ingrese el primer numero: ")
+num1=float(input())
+print("Ingrese el segundo numero: ")
+num2=float(input())
+
+divi= num1 / num2
+
+print("La división es: ",divi)
---
+++ multiplicacion.py
@@ -0,0 +1,8 @@
+print("Ingrese el primer numero: ")
+num1=float(input())
+print("Ingrese el segundo numero: ")
+num2=float(input())
+
+multi= num1 * num2
+
+print("La multiplicación es: ",multi)
